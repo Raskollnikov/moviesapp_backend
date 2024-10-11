ea199e4b48b14ca017f2a572413d05854468c418
Raskollnikov <bakhalaia56@gmail.com>
2024-10-12 02:01:30 +0400

    routes added and auth updated

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,34 +1,41 @@
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
-
 import asyncHandler from "./asyncHandler.js";
 
-// is user authenticated or not
-
+// Check if the user is authenticated
 const authenticated = asyncHandler(async (req, res, next) => {
-  let token;
-  token = req.cookies.jwt;
+  const token = req.cookies.jwt;
 
   if (token) {
     try {
+      // Verify the token and decode userId
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = await User.findById(decoded.userId.select("-password"));
+
+      // Find user by ID, excluding the password field
+      req.user = await User.findById(decoded.userId).select("-password");
+
+      if (!req.user) {
+        res.status(401);
+        throw new Error("User not found");
+      }
+
       next();
     } catch (error) {
       res.status(401);
-      throw new Error("not authorized, no token found");
+      throw new Error("Not authorized, token failed");
     }
   } else {
     res.status(401);
-    throw new Error("No Token, not authorized");
+    throw new Error("No token, not authorized");
   }
 });
 
+// Check if the user is an admin
 const authorizeAdmin = (req, res, next) => {
   if (req.user && req.user.isAdmin) {
     next();
   } else {
-    res.status(401).send("Not Authorized as an admin");
+    res.status(401).send("Not authorized as admin");
   }
 };
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,12 +3,27 @@ import {
   createUser,
   loginUser,
   logoutCurrentUser,
+  getAllUsers,
+  getCurrentUserProfile,
+  updateCurrentUserProfile,
 } from "../controllers/userController.js";
 
+import {
+  authenticated,
+  authorizeAdmin,
+} from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
-router.route("/").post(createUser);
+router
+  .route("/")
+  .post(createUser)
+  .get(authenticated, authorizeAdmin, getAllUsers);
 router.post("/auth", loginUser);
 router.post("/logout", logoutCurrentUser);
+
+router
+  .route("/profile")
+  .get(authenticated, getCurrentUserProfile)
+  .put(authenticated, updateCurrentUserProfile);
 
 export default router;
